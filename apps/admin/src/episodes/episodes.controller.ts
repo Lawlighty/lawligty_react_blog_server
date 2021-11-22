c13a6b0caf51bf83212f1f50a094b94b51582a3f
Lawlighty_li <Lawlighty_li@southbaytech.com>
2021-11-22 22:21:55 +0800

    Update episodes.controller.ts

--- apps/admin/src/episodes/episodes.controller.ts
+++ apps/admin/src/episodes/episodes.controller.ts
@@ -1,5 +1,6 @@
+import { Course } from '@libs/db/models/course.model';
 import { Episode } from '@libs/db/models/episode.model';
-import { Body, Controller, Put } from '@nestjs/common';
+import { Body, Controller, Delete, Post, Put, Param } from '@nestjs/common';
 import { ApiOperation, ApiTags } from '@nestjs/swagger';
 import { ReturnModelType } from '@typegoose/typegoose';
 import { Crud } from 'nestjs-mongoose-crud';
@@ -38,7 +39,52 @@ export class EpisodesController {
   constructor(
     @InjectModel(Episode)
     private readonly model: ReturnModelType<typeof Episode>,
+    @InjectModel(Course)
+    private readonly courseModel: ReturnModelType<typeof Course>,
   ) {}
+
+  @Post()
+  @ApiOperation({ summary: '添加课时' })
+  async addEpisodes(@Body() dto) {
+    // 创建课时
+    const res = await this.model.create(dto);
+    // 修改课程
+    const course = await this.courseModel.findById(dto.course);
+    let episodeList = course?.episodes ?? [];
+    episodeList.push(res._id);
+    await this.courseModel.updateOne(
+      { _id: dto.course },
+      { $set: { episodes: episodeList } },
+    );
+    return res;
+    // const episodeslist = await this.model.updateMany(
+    //   { _id: { $in: dto.idList } },
+    //   { $set: { course: dto.course_id } },
+    // );
+    // return { status: 200, data: episodeslist };
+  }
+
+  @Delete(':id')
+  @ApiOperation({ summary: '删除' })
+  async deleteEpisodes(@Param('id') id: string) {
+    // 创建课时
+    const res = await this.model.findByIdAndDelete(id);
+    if (res._id) {
+      // 修改课程
+      const course = await this.courseModel.findById(res.course);
+      let episodeList = course?.episodes ?? [];
+      let index = episodeList.findIndex(
+        (item) =>item&& item.toString() == res._id.toString(),
+      );
+      episodeList.splice(index, 1);
+      await this.courseModel.updateOne(
+        { _id: course._id },
+        { $set: { episodes: episodeList } },
+      );
+    }
+    
+    return res;
+  }
 
   @Put('updatepisodes')
   @ApiOperation({ summary: '修改课时的课程字段' })
